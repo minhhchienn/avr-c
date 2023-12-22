655fb40a830c834793ed8bda40717c3e54df4935
Minh Chien Giap <103621332+minhhchienn@users.noreply.github.com>
2023-12-23 03:34:25 +0700

    Update lcd1602.c

--- lib/lcd1602/lcd1602.c
+++ lib/lcd1602/lcd1602.c
@@ -11,8 +11,8 @@
 #include <util/delay.h>
 
 //Instruction set
-#define LCD_CLEAR				0x01			//Clear screen
-#define LCD_HOME				0x02			//Go to first if line one
+#define LCD_CLEAR			0x01			//Clear screen
+#define LCD_HOME			0x02			//Go to first if line one
 #define LCD_SHIFT_LEFT			0x04			//Left-shift the cursor after writing a character
 #define LCD_SHIFT_RIGHT			0x06			//Right-shift the cursor after writing a character
 #define LCD_DISPLAY_OFF			0x08			//Turn off the display
@@ -27,7 +27,7 @@ void lcd_send_command(uint8_t command){
 	LCD_CTRL_PORT &= ~(1 << LCD_RS);			//Set RS = 0 (command)
 	LCD_CTRL_PORT &= ~(1 << LCD_RW);			//Set RS = 0 (write)
 	LCD_CTRL_PORT |= (1 << LCD_E);				//Pull up enable
-	_delay_us(1);								//Delay at least 450ns
+	_delay_us(1);						//Delay at least 450ns
 	LCD_CTRL_PORT &= ~(1 << LCD_E);				//Pull down enable
 	_delay_ms(1);
 }
@@ -37,13 +37,13 @@ void lcd_send_data(uint8_t data){
 	LCD_CTRL_PORT |= 1 << LCD_RS;			//Set RS = 1 (data)
 	LCD_CTRL_PORT &= ~(1 << LCD_RW);			//Set RS = 0 (write)
 	LCD_CTRL_PORT |= (1 << LCD_E);				//Pull up enable
-	_delay_us(1);								//Delay at least 450ns
+	_delay_us(1);						//Delay at least 450ns
 	LCD_CTRL_PORT &= ~(1 << LCD_E);				//Pull down enable
 	_delay_ms(1);
 }
 
 void lcd_init(){
-	_delay_ms(15);								//Delay at least 15ms
+	_delay_ms(15);							//Delay at least 15ms
 	lcd_send_command(LCD_HOME);
 	lcd_send_command(0x38);						//8 bits mode, 2 line, 5x10 pixels per character
 	lcd_send_command(LCD_DISPLAY_ON);
